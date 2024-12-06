82f25483fe1dc5feefd6f23a94a62a1afda417a7
LookItsAndy <59989200+LookItsAndy@users.noreply.github.com>
2024-12-06 13:31:00 -0800

    wrote distance_reader.c

---
+++ Final Project/distance_reader.c
@@ -0,0 +1,61 @@
+#include "pca9665/pca9665.h"
+#include <wiringPi.h>
+#include <stdlib.h>
+#include <string.h>
+#define PIN_BASE 300
+#define MAX_PWM 4096
+#define HERTZ 50
+
+#define TRIG 28 //wPi#28=BCM GPIO#20=Physical pin#38
+#define ECHO 29 //wPi#29=BCM GPIO#21=Physical pin#40
+#define TARGET_DISTANCE 10 //10cm in front the ultra sonic sensor is the target
+
+
+
+int distance() {
+        //Send trig pulse
+        digitalWrite(TRIG, HIGH);
+        delayMicroseconds(20);
+        digitalWrite(TRIG, LOW);
+ 
+        //Wait for echo start
+        while(digitalRead(ECHO) == LOW);
+ 
+        //Wait for echo end
+        long startTime = micros();
+        while(digitalRead(ECHO) == HIGH);
+        long travelTime = micros() - startTime;
+ 
+        //Get distance in cm
+        int distance = travelTime / 58;
+ 		if (distance==0) distance=1000;
+        printf("Distance is: " distance);
+        return distance;
+}
+
+
+void main(void) {
+
+    if(wiringPiSetup()==-1){
+            printf("setup wiringPi failed!\n");
+            printf("please check your setup\n");
+            return -1;
+        }
+        setup();
+
+        printf("Part 1: Object Tracking");
+
+        int fd = pca9685Setup(PIN_BASE, 0x40, HERTZ);
+	if (fd < 0)
+	{
+		printf("Error in setup\n");
+		return fd;
+	}
+
+
+    while(true) {
+        distance();
+    }
+
+
+}
