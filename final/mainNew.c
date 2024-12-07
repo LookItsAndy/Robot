97fdbf634b017222519b20ae664632f1bc1cfff7
LookItsAndy <59989200+LookItsAndy@users.noreply.github.com>
2024-12-06 16:38:24 -0800

    Update mainNew.c
    Co-Authored-By: EthanGrants <105686621+EthanGrants@users.noreply.github.com>
    Co-Authored-By: sram13 <20263547+sram13@users.noreply.github.com>

--- final/mainNew.c
+++ final/mainNew.c
@@ -115,9 +115,9 @@ int main(void) {
         }
         setup();
 
-        printf("Part 1: Object Tracking");
+        printf("Part 1: Object Tracking\n");
 
-        int fd = pca9685Setup(PIN_BASE, 0x40, HERTZ);
+    
     if (fd < 0)
     {
         printf("Error in setup\n");
